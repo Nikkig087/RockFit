4626b5240edd9f9090503eebf3fea718922ffffe
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-20 19:53:41 +0000

    performed first migration with cloudinary set up

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+os.environ.setdefault(
+"DATABASE_URL", "postgres://u9qbmrznws6:tPoCTc6ovKLB@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/both_raid_daily_38548")
+os.environ.setdefault("SECRET_KEY", "<my_secret_key>")
+
+CLOUDINARY_STORAGE = {
+'CLOUDINARY_CLOUD_NAME':'dvgozeo62',
+'CLOUDINARY_API_KEY':'877696538918354',
+'CLOUDINARY_API_SECRET':'83XoStnIJI0Ux0Snby6soXqGmaE'
+}
+
